855ece269cc60fea79c9955613c40caab4868437
newfoundgeo <gis@newfoundgeo.com>
2013-11-05 22:30:34 -0800

    adding new clip_data.py

---
+++ scripts/clip_data.py
@@ -0,0 +1,148 @@
+"""----------------------------------------------------------------------------
+Name:        clip_data.py
+Purpose:     Copies data to a new or existing geodatabase.
+Author:
+Created:     01/10/2013
+Copyright:
+----------------------------------------------------------------------------"""
+import os
+import zipfile
+import arcpy
+
+
+def zip_data(file_gdb, name):
+    """Creates a compressed zip file for the geodatabase folder."""
+    zfile = os.path.join(os.path.dirname(file_gdb), name)
+    with zipfile.ZipFile(zfile, 'w', zipfile.ZIP_DEFLATED) as z:
+        for root, dirs, files in os.walk(file_gdb):
+            for f in files:
+                if not f.endswith('zip'):
+                    absf = os.path.join(root, f)
+                    zf = absf[len(file_gdb) + len(os.sep):]
+                    z.write(absf, os.path.join(os.path.basename(file_gdb), zf))
+# End zip_data function
+
+def clip_mxd_layers(mxd_path, coord_sys, geo_poly):
+    """Walks each feature and raster layer and clips it."""
+    mxd = arcpy.mapping.MapDocument(mxd_path)
+    layers = arcpy.mapping.ListLayers(mxd)
+    for layer in layers:
+        if coord_sys == '' or coord_sys == '#':
+            out_sr = arcpy.Describe(layer).spatialReference
+        else:
+            out_sr = arcpy.SpatialReference(int(coord_sys))
+        if not geo_poly.spatialReference.name == out_sr.name:
+            geo_transformation = arcpy.ListTransformations(geo_poly.spatialReference, out_sr)[0]
+            clip_poly = geo_poly.projectAs(out_sr, geo_transformation)
+        else:
+            clip_poly = geo_poly.projectAs(out_sr)
+
+        if layer.isFeatureLayer:
+            arcpy.analysis.Clip(layer.dataSource, clip_poly, layer.name)
+        elif layer.isRasterLayer:
+            ext = clip_poly.extent
+            arcpy.management.Clip(layer.dataSource, '{} {} {} {}'.format(ext.XMin, ext.YMin, ext.XMax, ext.YMax), layer.name)
+# End clip_mxd_layers function
+
+def clip_data(datasets,
+                out_gdb,
+                clip_area='',
+                out_coordinate_system='',
+                zipup=False):
+    """Clips data to a new or existing geodatabase.
+    Optionally, the geodatbase can be zipped into
+    a distributable file.
+    """
+    # Create the clip polygon.
+    gcs_sr = arcpy.SpatialReference(4326)
+    xys = clip_area.split()
+    points = arcpy.Array([
+                        arcpy.Point(xys[0], xys[1]),
+                        arcpy.Point(xys[0], xys[3]),
+                        arcpy.Point(xys[2], xys[3]),
+                        arcpy.Point(xys[2], xys[1])
+                        ])
+    gcs_clip_poly = arcpy.Polygon(points, gcs_sr)
+
+    if not os.path.exists(out_gdb):
+        out_gdb = arcpy.management.CreateFileGDB(os.path.dirname(out_gdb),
+                                                os.path.basename(out_gdb))[0]
+    arcpy.env.workspace = out_gdb
+    datasets = datasets.split(';')
+    for ds in datasets:
+        try:
+            dsc = arcpy.Describe(ds)
+            if not dsc.dataType == 'File' and not dsc.dataType == 'MapDocument':
+                if out_coordinate_system == '' or out_coordinate_system == '#':
+                    out_sr = dsc.spatialReference
+                else:
+                    out_sr = arcpy.SpatialReference(int(out_coordinate_system))
+                if not gcs_sr.name == out_sr.name:
+                    geo_transformation = arcpy.ListTransformations(gcs_sr, out_sr)[0]
+                    clip_poly = gcs_clip_poly.projectAs(out_sr, geo_transformation)
+                else:
+                    clip_poly = gcs_clip_poly.projectAs(out_sr)
+
+            if dsc.dataType == 'FeatureClass':
+                arcpy.analysis.Clip(ds, clip_poly, dsc.name)
+            elif dsc.dataType == 'ShapeFile':
+                arcpy.analysis.Clip(ds, clip_poly, dsc.name[:-4])
+            elif dsc.dataType == 'FeatureDataset':
+                arcpy.env.workspace = ds
+                for fc in arcpy.ListFeatureClasses():
+                    arcpy.analysis.Clip(fc, clip_poly, os.path.join(out_gdb, fc))
+                arcpy.env.workspace = out_gdb
+            elif dsc.dataType == 'RasterDataset':
+                ext = clip_poly.extent
+                arcpy.management.Clip(ds, '{} {} {} {}'.format(ext.XMin, ext.YMin, ext.XMax, ext.YMax), dsc.name)
+            elif dsc.dataType == 'Layer':
+                layer_from_file = arcpy.mapping.Layer(dsc.catalogPath)
+                layers = arcpy.mapping.ListLayers(layer_from_file)
+                for layer in layers:
+                    if layer.isFeatureLayer:
+                        arcpy.analysis.Clip(layer.dataSource, clip_poly, layer.name)
+                    elif layer.isRasterLayer:
+                        ext = clip_poly.extent
+                        arcpy.management.Clip(layer.dataSource, '{} {} {} {}'.format(ext.XMin, ext.YMin, ext.XMax, ext.YMax), layer.name)
+            elif dsc.dataType == 'CadDrawingDataset':
+                arcpy.env.workspace = dsc.catalogPath
+                cad_wks_name = os.path.splitext(dsc.name)[0]
+                for cad_fc in arcpy.ListFeatureClasses():
+                    if cad_fc.lower() == 'annotation':
+                        cad_anno = arcpy.conversion.ImportCADAnnotation(cad_fc, arcpy.CreateUniqueName('cadanno', arcpy.env.scratchGDB))
+                        arcpy.analysis.Clip(cad_anno, clip_poly, os.path.join(cad_wks_name, cad_fc))
+                    arcpy.analysis.Clip(cad_fc, clip_poly, os.path.join(cad_wks_name, cad_fc))
+                arcpy.env.workspace = out_gdb
+            elif dsc.dataType == 'File':
+                if dsc.catalogPath.endswith('.kml') or dsc.catalogPath.endswith('.kmz'):
+                    name = os.path.splitext(dsc.name)[0]
+                    kml_layer = arcpy.conversion.KMLToLayer(dsc.catalogPath, arcpy.env.scratchFolder, name)
+                    group_layer = arcpy.mapping.Layer(os.path.join(arcpy.env.scratchFolder, '{}.lyr'.format(name)))
+                    for layer in arcpy.mapping.ListLayers(group_layer):
+                        if layer.isFeatureLayer:
+                            arcpy.analysis.Clip(layer, gcs_clip_poly, arcpy.ValidateTableName(layer))
+                    del group_layer, layer
+                    # Clean up temp KML results.
+                    arcpy.management.Delete(os.path.join(arcpy.env.scratchFolder, '{}.lyr'.format(name)))
+                    arcpy.management.Delete(kml_layer[1])
+            elif dsc.dataType == 'MapDocument':
+                clip_mxd_layers(dsc.catalogPath, out_coordinate_system, gcs_clip_poly)
+
+        # Continue if an error. Process as many as possible.
+        except Exception as ex:
+            arcpy.AddWarning(ex)
+            continue
+
+    if zipup == True:
+        zip_name = os.path.splitext(os.path.basename(out_gdb))[0]
+        zip_data(out_gdb, '{}.zip'.format(zip_name))
+# End copy_data function
+
+if __name__ == '__main__':
+    input_datasets = arcpy.GetParameterAsText(0)
+    output_geodatabase = arcpy.GetParameterAsText(1)
+    clip_extent = arcpy.GetParameterAsText(2)
+    out_cs = arcpy.GetParameterAsText(3)
+    zip_up = arcpy.GetParameter(4)
+    clip_data(input_datasets, output_geodatabase, clip_extent, out_cs, zip_up)
+
