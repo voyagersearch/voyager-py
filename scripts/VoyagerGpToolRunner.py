a4088d5e77544376152dfe10736bc1718ee423e3
newfoundgeo <gis@newfoundgeo.com>
2013-12-26 09:36:05 -0800

    Updated to look for keys by name not index

--- scripts/VoyagerGpToolRunner.py
+++ scripts/VoyagerGpToolRunner.py
@@ -10,6 +10,12 @@ import sys
 import json
 import arcpy
 
+def find(f, seq):
+  """Return first item in sequence where f(item) == True."""
+  for item in seq:
+    if f(item):
+      return item
+
 def run_job(json_file):
     """Run a Geoprocessing tool.
     Parse the json for tool name and parameters.
@@ -33,23 +39,31 @@ def run_job(json_file):
 
         # TODO: this script makes assumptions about the *order* of the path elements
         # ideally this should lookup names by key and validate as required
+        if os.path.basename(request['task']) == 'ClipData':
 
-        # Prepare parameters for task.
-        in_data = ';'.join([v['lyr'] if not v['lyr'] == '' else v['path'] for v in parameters[1]['value']])
-        clip_geom_wkt = parameters[0]['wkt']
-        sr_code = parameters[2]['value']
-        if parameters[3]['value'] == 'FileGDB':
-            output_location = os.path.join(data['folder'], 'output.gdb')
-        else:
-            output_location = data['folder']
+            # Retrieve input items to be clipped.
+            input_items = find(lambda p: p['name'] == 'input_items', parameters)
+            in_data = ';'.join([v['lyr'] if not v['lyr'] == '' else v['path'] for v in input_items['value']])
 
-        # Execute task.
-        try:
-            if os.path.basename(request['task']) == 'ClipData':
-                arcpy.voyager.ClipData(in_data, output_location, clip_geom_wkt, sr_code)
-        except arcpy.ExecuteError:
-            sys.stderr.write(arcpy.GetMessages(2))
-            sys.exit(1)
+            # Retrieve clip geometry.
+            clip_geom_wkt = find(lambda p: p['name'] == 'clip_geometry', parameters)['wkt']
+
+            # Retieve the coordinate system code.
+            find(lambda p: p['name'] == 'output_projection', parameters)['value']
+
+            # Retrive the output format type.
+            output_format = find(lambda p: p['name'] == 'output_format', parameters)['value']
+            if output_format == 'FileGDB':
+                output_location = os.path.join(data['folder'], 'output.gdb')
+            else:
+                output_location = data['folder']
+
+            # Execute task.
+            try:
+                arcpy.voyager.ClipData(in_data, output_location, clip_geom_wkt, sr_code, out)
+            except arcpy.ExecuteError:
+                sys.stderr.write(arcpy.GetMessages(2))
+                sys.exit(1)
 
         # Success
         sys.exit(0)
