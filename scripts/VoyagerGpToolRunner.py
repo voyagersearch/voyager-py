02fd9e7c247e45a1ac94731a140c9b02ec206355
newfoundgeo <gis@newfoundgeo.com>
2014-01-13 20:02:33 -0800

    New function for getting items
    Tries to first use the local layer file and if that fails, downloads the
    layer file using layerfile url, and if that fails uses the dataset path.

--- scripts/VoyagerGpToolRunner.py
+++ scripts/VoyagerGpToolRunner.py
@@ -1,13 +1,15 @@
 """---------------------------------------------------------------------------
 Name:        VoyagerGpToolRunner.py
-Purpose:
+Purpose:     Execute the Voyager GP tool.
 Author:      VoyagerSearch
-Created:     18/12/2013
+Created:     12/18/2013
+Updated:     01/13/2014
 Copyright:   (c) VoyagerSearch 2013
 ---------------------------------------------------------------------------"""
 import os
 import sys
 import json
+import urllib
 import arcpy
 
 def find(f, seq):
@@ -16,17 +18,36 @@ def find(f, seq):
     if f(item):
       return item
 
+def get_item(item):
+    """Return a valid layer file or dataset path.
+
+    GetCount will fail if the layer file does not exist or
+    if the layer's datasource does not exist.
+    """
+    try:
+        cnt = arcpy.management.GetCount(item['lyrFile'])
+        return item['lyrFile']
+    except arcpy.ExecuteError:
+        pass
+
+    try:
+        layer_file = urllib.urlretrieve(item['lyrURL'])[0]
+        cnt = arcpy.management.GetCount(layer_file)
+        return layer_file
+    except Exception:
+        return item['path']
+# End get_item function
+
 def run_job(json_file):
     """Run a Geoprocessing tool.
     Parse the json for tool name and parameters.
     """
     with open(json_file) as data_file:
-        data = json.load(data_file)
-        request = data;  # data['request'][0]
+        request = json.load(data_file)
 
         # Load the GP Toolbox
         arcpy.ImportToolbox(os.path.dirname(request['task']))
-        
+
         # Create the folder if it does not exist.
         if not os.path.exists(data['folder']):
             os.makedirs(data['folder'])
@@ -34,14 +55,14 @@ def run_job(json_file):
         # Retrieve the list of parameters.
         parameters = request['params']
 
-        # TODO: this script makes assumptions about the *order* of the path elements
-        # ideally this should lookup names by key and validate as required
+        # Lookup names by key and validate as required.
         if os.path.basename(request['task']) == 'ClipData':
 
             # Retrieve input items to be clipped.
             input_items = find(lambda p: p['name'] == 'input_items', parameters)
             docs = input_items.get('response').get('docs')
-            in_data = ';'.join([v['[lyrFile]'] if not v['[lyrFile]'] == '' else v['path'] for v in docs])
+            in_data = ';'.join([get_item(v) for v in docs])
+            #in_data = ';'.join([v['[lyrFile]'] if not v['[lyrFile]'] == '' else v['path'] for v in docs])
 
             # Retrieve clip geometry.
             clip_geom_wkt = find(lambda p: p['name'] == 'clip_geometry', parameters)['wkt']
