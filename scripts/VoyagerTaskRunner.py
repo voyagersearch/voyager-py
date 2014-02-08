7306c2f7e6e4541d4f217059f28187341365b0c7
newfoundgeo <gis@newfoundgeo.com>
2014-02-07 18:22:08 -0800

    Updated to execute zip files process

--- scripts/VoyagerTaskRunner.py
+++ scripts/VoyagerTaskRunner.py
@@ -1,16 +1,13 @@
-"""---------------------------------------------------------------------------
-Name:        VoyagerTaskRunner.py
-Purpose:     Execute a Voyager processing task.
-Author:      VoyagerSearch
-Created:     12/18/2013
-Updated:     01/31/2014
-Copyright:   (c) VoyagerSearch 2013
----------------------------------------------------------------------------"""
+"""Executes a Voyager processing task."""
 import os
 import sys
 import json
 import urllib
-import shutil
+
+
+__author__ = 'VoyagerSearch'
+__copyright__ = 'VoyagerSearch, 2014'
+__date__ = '02/07/2014'
 
 try:
   import arcpy
@@ -44,65 +41,79 @@ def get_feature_data(item):
 # End get_item function
 
 
-def get_file(item):
-  """TODO: Return a valid file.
-  Implement when JSON format finalized. 
-  Will require no dependency on arcpy!
-  """
-  pass
+def clip_data_task(request, parameters):
+    """Runs the clip data task."""
+    import clip_data
 
-def run_job(json_file):
-    """Run a Geoprocessing tool.
-    Parse the json for tool name and parameters.
-    """
+    # Retrieve input items to be clipped.
+    input_items = find(lambda p: p['name'] == 'input_items', parameters)
+    docs = input_items.get('response').get('docs')
+    in_data = dict((get_feature_data(v), v['name']) for v in docs)
+
+    # Retrieve clip geometry.
+    try:
+        clip_geom_wkt = find(lambda p: p['name'] == 'clip_geometry', parameters)['wkt']
+    except KeyError:
+        clip_geom_wkt = find(lambda p: p['name'] == 'clip_geometry', parameters)['feature']
+
+    # Retrieve the coordinate system code.
+    sr_code = find(lambda p: p['name'] == 'output_projection', parameters)['code']
+
+    # Retrieve the output format type.
+    output_format = find(lambda p: p['name'] == 'output_format', parameters)['value']
+    output_location = request['folder']
+
+    # Execute task.
+    try:
+        clip_data.clip_data(str(in_data), output_location, clip_geom_wkt, int(sr_code), output_format)
+    except Exception as ex:
+        sys.stderr.write(ex.message)
+        sys.stderr.flush()
+        sys.exit(1)
+# End clip_data_task function
+
+
+def zip_files_task(request, parameters):
+    """Runs the zip files task."""
+    import zip_files
+
+    # Retrieve input items to be clipped.
+    input_items = find(lambda p: p['name'] == 'input_items', parameters)
+    docs = input_items.get('response').get('docs')
+    in_data = [v['path'] for v in docs]
+    output_location = request['folder']
+
+    try:
+        zip_files.zip_files(in_data, output_location)
+    except Exception as ex:
+        sys.stderr.write(ex.message)
+        sys.stderr.flush()
+        sys.exit(1)
+# End zip_files function
+
+
+def run_task(json_file):
+    """Main function for running processing tasks."""
     with open(json_file) as data_file:
         request = json.load(data_file)
 
         # Create the folder if it does not exist.
         if not os.path.exists(request['folder']):
-            os.makedirs(request['folder'])      
-                 
+            os.makedirs(request['folder'])
+
         # Retrieve the list of parameters.
         parameters = request['params']
 
-        # Lookup names by key and validate as required.
-        if os.path.basename(request['task']) == 'ClipData':
-            import clip_data
-
-            # Retrieve input items to be clipped.
-            input_items = find(lambda p: p['name'] == 'input_items', parameters)
-            docs = input_items.get('response').get('docs')
-            in_data = dict((get_feature_data(v), v['name']) for v in docs)
-
-            #in_data = [get_item(v) for v in docs]
-            #output_names = [n['name'] for n in docs]
-            #in_data = str(dict(zip(in_datasets, output_names)))
-
-            # Retrieve clip geometry.
-            try:
-                clip_geom_wkt = find(lambda p: p['name'] == 'clip_geometry', parameters)['wkt']
-            except KeyError:
-                clip_geom_wkt = find(lambda p: p['name'] == 'clip_geometry', parameters)['feature']
-
-            # Retieve the coordinate system code.
-            sr_code = find(lambda p: p['name'] == 'output_projection', parameters)['code']
-
-            # Retrive the output format type.
-            output_format = find(lambda p: p['name'] == 'output_format', parameters)['value']
-            output_location = request['folder']
-
-            # Execute task.
-            try:
-                clip_data.clip_data(str(in_data), output_location, clip_geom_wkt, int(sr_code), output_format)
-            except Exception as ex:
-                sys.stderr.write(ex.message)
-                sys.stderr.flush()
-                sys.exit(1)
+        if os.path.basename(request['task']) == 'clip_data':
+            clip_data_task(request, parameters)
+        elif os.path.basename(request['task']) == 'zip_files':
+            zip_files_task(request, parameters)
 
         # Success
         sys.exit(0)
+# End run_task function
 
 if __name__ == '__main__':
-    run_job(r"C:\GIS\Python\ClippingData\cliptask.json")
-    #run_job(sys.argv[1])
+    #run_job(r"C:\NewfoundGEO\Clients\Voyager\voyager-processing\scripts\zipfilestask.json")
+    run_task(sys.argv[1])
 
