752689fd86116fc54b5779461faa7e5df4e530b5
newfoundgeo <gis@newfoundgeo.com>
2014-03-04 13:53:02 -0800

    VG-545: Fixes for Add to geodatabase
    updated to support the gp task framework. Includes support for naming
    the outputs, reporting status, and allows tables as input.

--- scripts/add_to_geodatabase.py
+++ scripts/add_to_geodatabase.py
@@ -1,13 +1,14 @@
-"""----------------------------------------------------------------------------
-Name:        add_to_geodatabase.py
-Purpose:     Copies data to a new or existing geodatabase.
-Author:
-Created:     01/10/2013
-Copyright:
-----------------------------------------------------------------------------"""
+"""Copies data to a new or existing geodatabase."""
+from __future__ import unicode_literals
 import os
 import zipfile
 import arcpy
+import status
+
+__author__ = 'VoyagerSearch'
+__copyright__ = 'VoyagerSearch, 2014'
+__date__ = '03/04/2014'
+
 
 def zip_data(file_gdb, name):
     """Creates a compressed zip file for the geodatabase folder."""
@@ -19,11 +20,9 @@ def zip_data(file_gdb, name):
                     absf = os.path.join(root, f)
                     zf = absf[len(file_gdb) + len(os.sep):]
                     z.write(absf, os.path.join(os.path.basename(file_gdb), zf))
-
-        z.write(os.path.join(os.path.dirname(file_gdb), 'output.log'), 'output.log')
-
     return zfile
 # End zip_data function
+
 
 def create_unique_name(name, gdb):
     """Creates a valid and unique name for the geodatabase."""
@@ -32,53 +31,73 @@ def create_unique_name(name, gdb):
     return unique_name
 # End create_unique_name function
 
-def add_to_gdb(datasets, out_gdb, out_coordinate_system=''):
-    """Copys data to a new or existing geodatabase."""
-    log_file = open(os.path.join(os.path.dirname(out_gdb), 'output.log'), 'w')
+
+def add_to_gdb(input_items, output_workspace, out_coordinate_system=None):
+    """Copies data to a new or existing geodatabase."""
+    try:
+        # Voyager Job Runner: passes a dictionary of inputs and output names.
+        input_items = eval(input_items)
+    except SyntaxError:
+        # If not output names are passed in.
+        input_items = dict((k, '') for k in input_items.split(';'))
 
     # Create the geodatabase if it does not exist.
-    if not os.path.exists(out_gdb):
-        out_gdb = arcpy.management.CreateFileGDB(os.path.dirname(out_gdb),
-                                                os.path.basename(out_gdb))[0]
-        log_file.write('Created output geodatabase: {0}.\n'.format(out_gdb))
+    if not output_workspace.endswith('.gdb'):
+        out_gdb = arcpy.management.CreateFileGDB(output_workspace, 'output.gdb').getOutput(0)
     else:
-        log_file.write('Using existing geodatabase: {0}.\n'.format(out_gdb))
+        out_gdb = output_workspace
+    arcpy.env.workspace = out_gdb
 
     # Set the output coordinate system environment.
-    if not out_coordinate_system in ('', '#', None):
+    if out_coordinate_system is not None:
         arcpy.env.outputCoordinateSystem = int(out_coordinate_system)
-        log_file.write('Output coordinate system: {0}.\n'.format(arcpy.env.outputCoordinateSystem))
-    else:
-        log_file.write('Output coordinate system is same as inputs.\n')
 
-    arcpy.env.workspace = out_gdb
-    datasets = datasets.split(';')
-    for ds in datasets:
+    i = 1.
+    count = len(input_items)
+    status_writer = status.Writer()
+    status_writer.send_status('Starting to add data to {0}...'.format(out_gdb))
+    for ds, out_name in input_items.iteritems():
         try:
             dsc = arcpy.Describe(ds)
             if dsc.dataType == 'FeatureClass':
-                arcpy.management.CopyFeatures(ds, create_unique_name(dsc.name, out_gdb))
+                if out_name == '':
+                    arcpy.management.CopyFeatures(ds, create_unique_name(dsc.name, out_gdb))
+                else:
+                    arcpy.management.CopyFeatures(ds, create_unique_name(out_name, out_gdb))
             elif dsc.dataType == 'ShapeFile':
-                arcpy.management.CopyFeatures(ds, create_unique_name(dsc.name[:-4], out_gdb))
+                if out_name == '':
+                    arcpy.management.CopyFeatures(ds, create_unique_name(dsc.name[:-4], out_gdb))
+                else:
+                    arcpy.management.CopyFeatures(ds, create_unique_name(out_name, out_gdb))
             elif dsc.dataType == 'FeatureDataset':
                 fds = arcpy.management.CreateFeatureDataset(out_gdb, dsc.name)
                 arcpy.env.workspace = dsc.catalogPath
                 for fc in arcpy.ListFeatureClasses():
                     name = os.path.basename(create_unique_name(fc, out_gdb))
-                    arcpy.management.CopyFeatures(fc, os.path.join(fds[0], name))
+                    arcpy.management.CopyFeatures(fc, os.path.join(fds.getOutput(0), name))
                 arcpy.env.workspace = out_gdb
             elif dsc.dataType == 'RasterDataset':
-                arcpy.management.CopyRaster(ds, create_unique_name(dsc.name, out_gdb))
+                if out_name == '':
+                    arcpy.management.CopyRaster(ds, create_unique_name(dsc.name, out_gdb))
+                else:
+                    arcpy.management.CopyRaster(ds, create_unique_name(out_name, out_gdb))
             elif dsc.dataType == 'RasterCatalog':
-                arcpy.management.CopyRasterCatalogItems(ds, create_unique_name(dsc.name, out_gdb))
+                if out_name == '':
+                    arcpy.management.CopyRasterCatalogItems(ds, create_unique_name(dsc.name, out_gdb))
+                else:
+                    arcpy.management.CopyRasterCatalogItems(ds, create_unique_name(out_name, out_gdb))
             elif dsc.dataType == 'Layer':
                 layer_from_file = arcpy.mapping.Layer(dsc.catalogPath)
                 layers = arcpy.mapping.ListLayers(layer_from_file)
                 for layer in layers:
+                    if out_name == '':
+                        name = create_unique_name(layer.name, out_gdb)
+                    else:
+                        name = create_unique_name(out_name, out_gdb)
                     if layer.isFeatureLayer:
-                        arcpy.management.CopyFeatures(layer.dataSource, create_unique_name(layer.name, out_gdb))
+                        arcpy.management.CopyFeatures(layer.dataSource, name)
                     elif layer.isRasterLayer:
-                        arcpy.management.CopyRaster(layer.dataSource, create_unique_name(layer.name, out_gdb))
+                        arcpy.management.CopyRaster(layer.dataSource, name)
             elif dsc.dataType == 'CadDrawingDataset':
                 arcpy.env.workspace = dsc.catalogPath
                 cad_wks_name = os.path.splitext(dsc.name)[0]
@@ -93,7 +112,6 @@ def add_to_gdb(datasets, out_gdb, out_coordinate_system=''):
                     for layer in arcpy.mapping.ListLayers(group_layer):
                         if layer.isFeatureLayer:
                             arcpy.management.CopyFeatures(layer, arcpy.ValidateTableName(layer))
-                    del group_layer, layer
                     # Clean up temp KML results.
                     arcpy.management.Delete(os.path.join(arcpy.env.scratchFolder, '{}.lyr'.format(name)))
                     arcpy.management.Delete(kml_layer[1])
@@ -105,30 +123,25 @@ def add_to_gdb(datasets, out_gdb, out_coordinate_system=''):
                         arcpy.management.CopyFeatures(layer.dataSource, create_unique_name(layer.name, out_gdb))
                     elif layer.isRasterLayer:
                         arcpy.management.CopyRaster(layer.dataSource, create_unique_name(layer.name, out_gdb))
+            elif dsc.dataType.find('Table') > 0:
+                if out_name == '':
+                    arcpy.management.CopyRows(ds, create_unique_name(dsc.name, out_gdb))
+                else:
+                    arcpy.management.CopyRows(ds, create_unique_name(out_name, out_gdb))
 
-            arcpy.AddMessage('--Added {0} to {1}...'.format(ds, out_gdb))
-            log_file.write('--Added {0} to {1}.\n'.format(ds, out_gdb))
+            status_writer.send_percent(i/count, 'Added {0}.'.format(ds), 'add_to_geodatabase')
+            i += 1.
 
         # Continue if an error. Process as many as possible.
         except Exception as ex:
-            arcpy.AddWarning(ex)
-            log_file.write('--Failed to add: {0}...\n'.format(ds))
-            log_file.write('--Error: {0}.\n'.format(ex))
-            continue
-
-    log_file.flush()
-    log_file.close()
+            status_writer.send_percent(i/count,
+                                       '--Error: {0}.\n Failed to add: {1}.\n'.format(ex, ds),
+                                       'add_to_geodatabase')
+            pass
 
     # Zip the output gdb and log file.
     zip_name = os.path.splitext(os.path.basename(out_gdb))[0]
-    zf = zip_data(out_gdb, '{}.zip'.format(zip_name))
-    arcpy.SetParameterAsText(3, zf)
-
+    zip_data(out_gdb, '{0}.zip'.format(zip_name))
+    status_writer.send_status('Completed.')
 # End add_to_gdb function
-
-if __name__ == '__main__':
-    input_datasets = arcpy.GetParameterAsText(0)
-    output_geodatabase = arcpy.GetParameterAsText(1)
-    out_cs = arcpy.GetParameterAsText(2)
-    add_to_gdb(input_datasets, output_geodatabase, out_cs)
 
