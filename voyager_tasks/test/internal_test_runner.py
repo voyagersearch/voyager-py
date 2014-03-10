cca5494914a91ba5472626176c7c1e84f8e85e2e
jason <jason@voyagersearch.com>
2014-03-10 15:38:05 -0700

    test passes
    and cleaner runner.

--- voyager_tasks/test/internal_test_runner.py
+++ voyager_tasks/test/internal_test_runner.py
@@ -1,25 +1,26 @@
+"""Voyager unittest runner."""
 import os
 import glob
-import re
 import unittest
 import xmlrunner
 
 
 if __name__ == '__main__':
-    testLocation = os.getcwd()
-    os.mkdir(os.path.join(testLocation, 'test-reports'))
-    testPath = os.path.abspath(testLocation)
-    testScripts = glob.glob('{0}/test_*.py'.format(testLocation))
+    test_location = os.getcwd()
+    report_location = os.path.join(test_location, 'test-reports')
+    if not os.path.exists(report_location):
+        os.mkdir(report_location)
+    test_scripts = glob.glob('{0}/test_*.py'.format(test_location))
     scriptToModuleName = lambda f: os.path.splitext(os.path.basename(f))[0]
-    moduleNames = map(scriptToModuleName, testScripts)
-    modules = map(__import__, moduleNames)
+    module_names = map(scriptToModuleName, test_scripts)
+    modules = map(__import__, module_names)
     load = unittest.defaultTestLoader.loadTestsFromModule
 
     suite = unittest.TestSuite(map(load, modules))
-    i = 0
-    for uTest in suite._tests:
-        print 'Running test: %s' %testScripts[i]
-        with open(os.path.join(testLocation, 'test-reports', '{0}.xml'.format(uTest._tests[0]._tests[0].__module__)), 'w') as resultsLog:
-            xmlrunner.XMLTestRunner(resultsLog).run(uTest)
-        i+=1
+    for i, unit_test in enumerate(suite._tests, 0):
+        print 'Running test: {0}'.format(test_scripts[i])
+        test_case_name = unit_test._tests[0]._tests[0].__module__
+        with open(os.path.join(report_location, '{0}.xml'.format(test_case_name)), 'w') as results_log:
+            xmlrunner.XMLTestRunner(results_log).run(unit_test)
+
 
