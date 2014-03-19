c85d685e34a03e0cba16e6e1df0f075a4952db80
jason <jason@voyagersearch.com>
2014-03-19 08:05:25 -0700

    Cleaned up code

--- _update_build_number.py
+++ _update_build_number.py
@@ -1,22 +1,11 @@
 """Updates the build number in the README.md."""
 import os
-#import re
 
+
+build_num = os.environ['BUILD_NUMBER']
 readme_dir = os.getcwd()
-
-# with open(os.path.join(readme_dir, 'README.md'), 'r') as readme:
-#     for l in readme:
-#         if l.startswith('Build:'):
-#             result = re.findall(r'\d+', l)
-#             if result and isinstance(int(result[0]), int):
-#                 build_num = int(result[0])
-#                 new_build_num = build_num + 1
-#                 break
-build_num = int(os.environ['BUILD_NUMBER'])
-new_build_num = build_num + 1
-
 in_readme = open(os.path.join(readme_dir, 'README.md')).read()
 out_readme = open(os.path.join(readme_dir, 'README.md'), 'w')
-in_readme = in_readme.replace('BUILD_NUMBER', str(new_build_num))
+in_readme = in_readme.replace('BUILD_NUMBER', build_num)
 out_readme.write(in_readme)
 out_readme.close()
