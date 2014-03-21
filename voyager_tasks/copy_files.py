d2f9d338185872a2cb01eee91d8205d4a20cc429
jason <jason@voyagersearch.com>
2014-03-21 11:03:53 -0700

    VG-551: Copy Files task

---
+++ voyager_tasks/copy_files.py
@@ -0,0 +1,100 @@
+import os
+import glob
+import shutil
+from voyager_tasks.utils import status
+from voyager_tasks.utils import task_utils
+
+
+def get_files(source_file, file_extensions):
+    """Returns a list of files for each file type.
+    :param source_file: source file path
+    :param file_extensions: list of file extensions - i.e. ['*.shp', '*.prj']
+    :rtype : list
+    """
+    folder_location = os.path.dirname(source_file)
+    file_name = os.path.basename(source_file)[:-4]
+    all_files = []
+    for ext in file_extensions:
+        all_files.extend(glob.glob(os.path.join(folder_location, '{0}.{1}'.format(file_name, ext))))
+    return all_files
+
+
+def execute(request):
+    """Copies files data to a target folder.
+    :param request: json as a dict.
+    """
+    parameters = request['params']
+
+    in_data = task_utils.find(lambda p: p['name'] == 'input_items', parameters)
+    docs = in_data.get('response').get('docs')
+    input_items = [v['path'] for v in docs]
+
+    target_folder = task_utils.find(lambda p: p['name'] == 'target_folder', parameters)['value']
+
+    task_folder = request['folder']
+    if not os.path.exists(task_folder):
+        os.makedirs(task_folder)
+
+    try:
+        flatten_results = task_utils.find(lambda p: p['name'] == 'flatten_results', request['params'])['value']
+    except KeyError:
+        target_dirs = os.path.splitdrive(target_folder)[1]
+        flatten_results = 'false'
+
+    i = 1.
+    copied = 0
+    skipped = 0
+    file_count = len(input_items)
+    shp_files = ('shp', 'shx', 'sbn', 'dbf', 'prj', 'cpg', 'shp.xml', 'dbf.xml')
+    sdc_files = ('sdc', 'sdi', 'sdc.xml', 'sdc.prj')
+    status_writer = status.Writer()
+
+    for src_file in input_items:
+        try:
+            if os.path.isfile(src_file) or src_file.endswith('.gdb'):
+                if flatten_results == 'false':
+                    # Maintain source file's folder structure.
+                    copy_dirs = os.path.splitdrive(os.path.dirname(src_file))[1]
+                    if not copy_dirs == target_dirs:
+                        dst = target_folder + copy_dirs
+                        if not os.path.exists(dst):
+                            os.makedirs(dst)
+                else:
+                    if not os.path.exists(target_folder):
+                        dst = target_folder
+                        os.makedirs(dst)
+                if os.path.isfile(src_file):
+                    if src_file.endswith('.shp'):
+                        all_files = get_files(src_file, shp_files)
+                    elif src_file.endswith('.sdc'):
+                        all_files = get_files(src_file, sdc_files)
+                    else:
+                        all_files = [src_file]
+                    for f in all_files:
+                        shutil.copy2(f, dst)
+                else:
+                    shutil.copytree(src_file, os.path.join(dst, os.path.basename(src_file)))
+                status_writer.send_percent(i/file_count, 'Copied {0}.'.format(src_file), 'copy_files')
+                copied += 1
+            else:
+                status_writer.send_percent(
+                    i/file_count,
+                    '{0} is not a file or does not exist.'.format(src_file),
+                    'copy_files'
+                )
+                skipped += 1
+        except IOError as io_err:
+            status_writer.send_percent(
+                i/file_count,
+                'Failed to copy: {0}. {1}.'.format(src_file, repr(io_err)),
+                'copy_files'
+            )
+            skipped += 1
+            pass
+
+    shutil.copyfile(
+        os.path.join(os.path.dirname(__file__), r'supportfiles\_thumb.png'),
+        os.path.join(request['folder'], '_thumb.png')
+    )
+    task_utils.report(os.path.join(task_folder, '_report.md'), request['task'], copied, skipped)
+    
