fe9e6633ba5e01ad97631ec7eecd6ccca1b2935c
jason <jason@voyagersearch.com>
2014-04-14 20:44:21 -0700

    updated unittest for zipfiles

--- voyager_tasks/test/test_zip_files.py
+++ voyager_tasks/test/test_zip_files.py
@@ -24,20 +24,20 @@ class TestZipFilesTask(unittest.TestCase):
 
     def test_zip_files(self):
         """Testing the zip files task"""
-        base_kmz_files = ['whypython.txt', 'states.dbf', 'cities.kmz']
+        base_zip_files = ['whypython.txt', 'states.dbf', 'cities.kmz']
 
         text_file = os.path.join(os.getcwd(), 'test-data', 'whypython.txt')
         dbf_file = os.path.join(os.getcwd(), 'test-data', 'states.dbf')
         kml_file = os.path.join(os.getcwd(), 'test-data', 'cities.kmz')
-        non_file = os.path.join(os.getcwd(), 'test-data', 'emptyfolder')
+        #non_file = os.path.join(os.getcwd(), 'test-data', 'emptyfolder')
         self.request['params'][0]['response']['docs'][0]['path'] = text_file
         self.request['params'][0]['response']['docs'][1]['path'] = dbf_file
         self.request['params'][0]['response']['docs'][2]['path'] = kml_file
-        self.request['params'][0]['response']['docs'][3]['path'] = non_file
+        #self.request['params'][0]['response']['docs'][3]['path'] = non_file
         __import__(self.request['task'])
         getattr(sys.modules[self.request['task']], "execute")(self.request)
-        kmz_files = zipfile.ZipFile(os.path.join(self.temp_folder, 'output.zip')).namelist()
-        self.assertEqual(sorted(kmz_files), sorted(base_kmz_files))
+        zip_files = zipfile.ZipFile(os.path.join(self.temp_folder, 'output.zip')).namelist()
+        self.assertEqual(sorted(zip_files), sorted(base_zip_files))
 
 
 if __name__ == '__main__':
