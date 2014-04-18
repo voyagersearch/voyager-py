042f3acdd2bee9afee2114efd6831bc8eb235429
jason <jason@voyagersearch.com>
2014-04-18 09:35:42 -0700

    VG-1303: info files & task scripts for sprint 26
    Task scripts are just stubs at the moment.

---
+++ voyager_tasks/create_esri_package.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from voyager_tasks.utils import status
+from voyager_tasks.utils import task_utils
+
+
+def execute(request):
+    """Package inputs to an Esri map or layer package.
+    :param request: json as a dict.
+    """
+    pass
+
+
---
+++ voyager_tasks/replace_workspace_path.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from voyager_tasks.utils import status
+from voyager_tasks.utils import task_utils
+
+
+def execute(request):
+    """Replace the workspace path for layer files and map document layers.
+    :param request: json as a dict.
+    """
+    pass
