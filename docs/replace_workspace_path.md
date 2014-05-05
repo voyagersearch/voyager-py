4b11c1db4450a77f4a588bdb38bf8ff67fe92d63
jason <jason@voyagersearch.com>
2014-05-05 15:39:51 -0700

    Added  parameter for old workspace path
    new parameter where user enters the existing path "Old Workspace Path"

--- docs/replace_workspace_path.md
+++ docs/replace_workspace_path.md
@@ -1,12 +1,13 @@
 #Replace Workspace Path
 
-Replaces the workspace path for selected layer files and map document layers.
+Replaces an old workspace path for selected layer files and map document layers with a new workspace path.
 
 ###Usage Tips
   - The input search results must be layer files or map documents.
   - By default, back ups are created. Backups are created in the source location with a .bak extension. **RECOMMENDED**
   - The workspace will only be updated if the new workspace path is a valid workspace.
   For example, personal geodatabases can be replaced with file geodatabases. However, a geodatabase cannot be replaced by a folder.
+  - Partial paths can be updated. For example, a workspace path containing *C:\Data*, can be updated and replaced with *D:\Data*.
 
 ###Screen Captures
 ![Replace Workspace Path] (imgs/replace_workspace_path_0.png "Replace Workspace Path example")
--- voyager_tasks/replace_workspace_path.py
+++ voyager_tasks/replace_workspace_path.py
@@ -28,7 +28,8 @@ def execute(request):
     parameters = request['params']
     input_items = task_utils.get_input_items(parameters)
     backup = task_utils.get_parameter_value(parameters, 'create_backup', 'value')
-    new_workspace = task_utils.get_parameter_value(parameters, 'workspace_path', 'value')
+    old_workspace = task_utils.get_parameter_value(parameters, 'old_workspace_path', 'value').lower()
+    new_workspace = task_utils.get_parameter_value(parameters, 'new_workspace_path', 'value')
 
     if not os.path.exists(request['folder']):
         os.makedirs(request['folder'])
@@ -63,25 +64,31 @@ def execute(request):
             for layer in layers:
                 try:
                     if layer.isFeatureLayer:
-                        layer.replaceDataSource(new_workspace, workspace_type, validate=True)
+                        if layer.workspacePath.lower().find(old_workspace) > -1:
+                            new_source = layer.workspacePath.lower().replace(old_workspace, new_workspace)
+                            layer.replaceDataSource(new_source, workspace_type, validate=True)
                     elif layer.isRasterLayer:
-                        if not workspace_type == 'NONE':
-                            layer.replaceDataSource(new_workspace, workspace_type, validate=True)
-                        else:
-                            layer.replaceDataSource(new_workspace, 'RASTER_WORKSPACE', validate=True)
+                        if layer.workspacePath.lower().find(old_workspace) > -1:
+                            new_source = layer.workspacePath.lower().replace(old_workspace, new_workspace)
+                            if not workspace_type == 'NONE':
+                                layer.replaceDataSource(new_source, workspace_type, validate=True)
+                            else:
+                                layer.replaceDataSource(new_source, 'RASTER_WORKSPACE', validate=True)
                     if item.endswith('.lyr'):
                         layer.save()
                 except ValueError:
-                    status_writer.send_status('Workspace not valid for: {0}'.format(layer.datasetName))
+                    status_writer.send_status('Workspace is not valid type for: {0}'.format(layer.datasetName))
                     skipped += 1
                     pass
 
         if table_views:
             for table_view in table_views:
                 try:
-                    table_view.replaceDataSource(new_workspace, workspace_type, validate=False)
+                    if table_view.workspacePath.lower().find(old_workspace) > -1:
+                        new_source = table_view.workspacePath.lower().replace(old_workspace, new_workspace)
+                        table_view.replaceDataSource(new_source, workspace_type, validate=False)
                 except ValueError:
-                    status_writer.send_status('Workspace not valid for: {0}'.format(table_view.datasetName))
+                    status_writer.send_status('Workspace is not valid type for: {0}'.format(table_view.datasetName))
                     skipped += 1
                     pass
         if mxd:
@@ -92,10 +99,9 @@ def execute(request):
     try:
         shutil.copy2(os.path.join(os.path.dirname(__file__), 'supportfiles', '_thumb.png'), request['folder'])
     except IOError:
-        status_writer.send_status('Could not copy thumbnail.')
         pass
 
     # Update state if necessary.
     if skipped > 0:
-        status_writer.send_state(status.STAT_WARNING, 'Could not replace workspace for {0} results.'.format(skipped))
+        status_writer.send_state(status.STAT_WARNING, '{0} results could not be processed'.format(skipped))
     task_utils.report(os.path.join(request['folder'], '_report.json'), updated, skipped)
