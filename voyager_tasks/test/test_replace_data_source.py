bfe22340c34ae1806e93e9b10d31dcab98ccfe12
jason <jason@voyagersearch.com>
2014-07-14 13:10:27 -0700

    Replaced unit test for replace workspace path

--- voyager_tasks/test/test_replace_data_source.py
+++ voyager_tasks/test/test_replace_data_source.py
@@ -9,11 +9,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 import arcpy
 
 
-class TestReplaceWorkspacePath(unittest.TestCase):
-    """Test case for Replace to Workspace task."""
+class TestReplaceDataSource(unittest.TestCase):
+    """Test case for Replace Data Source task."""
     @classmethod
     def setUpClass(self):
-        with open(os.path.join(os.getcwd(), 'replace_workspace_path.test.json')) as data_file:
+        with open(os.path.join(os.getcwd(), 'replace_data_source.test.json')) as data_file:
             self.request = json.load(data_file)
         self.temp_folder = tempfile.mkdtemp()
         self.request['folder'] = self.temp_folder
@@ -28,13 +28,13 @@ class TestReplaceWorkspacePath(unittest.TestCase):
         lyr_file = os.path.join(self.temp_folder, 'Cities.lyr')
         self.request['params'][0]['response']['docs'][0]['[lyrFile]'] = lyr_file
         __import__(self.request['task'])
-        old_workspace = "C:\\GISData\\MDB\\USA.mdb"
-        new_workspace = os.path.join(os.getcwd(), 'test-data', 'TestData_v10.gdb')
+        old_workspace = "C:\\GISData\\MDB\\USA.mdb\\cities"
+        new_workspace = os.path.join(os.getcwd(), 'test-data', 'TestData_v10.gdb', 'cities')
         self.request['params'][2]['value'] = old_workspace
         self.request['params'][3]['value'] = new_workspace
         getattr(sys.modules[self.request['task']], "execute")(self.request)
         dsc = arcpy.Describe(lyr_file)
-        self.assertEquals(dsc.featureclass.catalogpath, os.path.join(new_workspace, 'cities'))
+        self.assertEquals(dsc.featureclass.catalogpath, os.path.join(os.path.dirname(new_workspace), 'cities'))
 
     def test_layer_backup_exists(self):
         """Test if backup (.bak) file was created"""
