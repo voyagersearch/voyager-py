c0afe318895ed0c3371c1cb151180ff01e499527
jason <jason@voyagersearch.com>
2014-07-23 08:02:06 -0700

    cleanup of file

--- voyager_worker/__init__.py
+++ voyager_worker/__init__.py
@@ -3,14 +3,14 @@ import sys
 import platform
 import glob
 
-__all__ = ['base_job', 'esri_worker', 'gdal_worker', 'odbc_worker', 'voyager_utils']
+__all__ = ['base_job', 'esri_worker', 'gdal_worker', 'odbc_worker', 'voyager_utils', 'status']
 
 # Add dependent libraries to the system paths.
 if platform.system() == 'Darwin':
     dll_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'arch', 'darwin_x86_64'))
     os.environ['PATH'] += os.pathsep + dll_path
 else:
-    dll_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'arch', 'win32_x86')) #r"C:\Voyager\server_1.9.4.1815\app\arch\win32_x86"
+    dll_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'arch', 'win32_x86'))  # r"C:\Voyager\server_1.9.4.1815\app\arch\win32_x86"
     if os.environ['PATH'].endswith(';'):
         os.environ['PATH'] += dll_path
     else:
@@ -19,8 +19,3 @@ egg_path = os.path.join(dll_path, 'py')
 libs = glob.glob(os.path.join(egg_path, '*.egg'))
 for lib in libs:
     sys.path.append(lib)
-
-
-
-
-
