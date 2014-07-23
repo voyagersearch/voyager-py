bb0c53a32fcfc8d9192cffd101a493abcee2d6cf
jason <jason@voyagersearch.com>
2014-07-23 07:57:01 -0700

    Fixed bug to report correct geometry information

--- voyager_worker/oracle_worker.py
+++ voyager_worker/oracle_worker.py
@@ -35,7 +35,7 @@ def global_job(args):
 def worker():
     """Worker function to index each row in each table in the database."""
     job.connect_to_zmq()
-    job.connect_to_database('Oracle')
+    job.connect_to_database()
     tables = []
 
     if not job.tables_to_keep == ['*']:
@@ -47,51 +47,73 @@ def worker():
 
     # cur.execute("select column_name from all_tab_cols where table_name = 'FC_COUNTRIES' AND column_name like 'CNTRY%'")
     for tbl in set(tables):
-        geo = {}
-        has_shape = False
-        is_point = False
-        columns = []
-        if not job.fields_to_keep == ['*']:
-            for col in job.fields_to_keep:
-                qry = "SELECT COLUMN_NAME FROM all_tab_cols WHERE table_name = '{0}' AND column_name LIKE '{1}'".format(tbl, col)
-                [columns.append(c[0]) for c in job.execute_query(qry).fetchall()]
-        else:
-            job.db_cursor.execute("SELECT * FROM {0}".format(tbl))
-            [columns.append(c[0]) for c in job.db_cursor.description]
+        try:
+            i = 0
+            geo = {}
+            has_shape = False
+            is_point = False
+            columns = []
+            if not job.fields_to_keep == ['*']:
+                for col in job.fields_to_keep:
+                    qry = "SELECT COLUMN_NAME FROM all_tab_cols WHERE table_name = '{0}' AND column_name LIKE '{1}'".format(tbl, col)
+                    [columns.append(c[0]) for c in job.execute_query(qry).fetchall()]
+            else:
+                job.db_cursor.execute("SELECT * FROM {0}".format(tbl))
+                [columns.append(c[0]) for c in job.db_cursor.description]
 
-        #[columns.remove(c) for c in columns if c.startswith('SYS_')]
+            #[columns.remove(c) for c in columns if c.startswith('SYS_')]
 
-        if 'SHAPE' in columns:
-            has_shape = True
-            columns.remove('SHAPE')
-            schema = job.db_cursor.execute("select SHAPE  from {0}".format(tbl)).fetchone()[0].type.schema
-            shape_type = job.db_cursor.execute("select {0}.ST_GEOMETRYTYPE(SHAPE) from {1}".format(schema, tbl)).fetchone()[0]
-            if 'POINT' in shape_type:
-                is_point = True
-
-        rows = job.db_cursor.execute("select {0} from {1}".format(','.join(columns), tbl)).fetchall()
-        for row in rows:
-            entry = {}
-            if has_shape:
-                if is_point:
-                    xcoord, ycoord = job.db_cursor.execute("select {0}.st_x(SHAPE), {0}.st_y(SHAPE) from {1}".format(schema, tbl)).fetchone()
-                    geo['lon'] = xcoord
-                    geo['lat'] = ycoord
+            if 'SHAPE' in columns:
+                has_shape = True
+                columns.remove('SHAPE')
+                schema = job.db_cursor.execute("select SHAPE  from {0}".format(tbl)).fetchone()[0].type.schema
+                shape_type = job.db_cursor.execute("select {0}.ST_GEOMETRYTYPE(SHAPE) from {1}".format(schema, tbl)).fetchone()[0]
+                if 'POINT' in shape_type:
+                    columns.insert(0, '{0}.st_y(SHAPE)'.format(schema, tbl))
+                    columns.insert(0, '{0}.st_x(SHAPE)'.format(schema, tbl))
+                    is_point = True
                 else:
-                    xmin, ymin, xmax, ymax = job.db_cursor.execute("select {0}.st_minx(SHAPE),{0}.st_miny(SHAPE),{0}.st_maxx(SHAPE), {0}.st_maxy(SHAPE) from {1}".format(schema, tbl)).fetchone()
-                    geo['xmin'] = xmin
-                    geo['ymin'] = ymin
-                    geo['xmax'] = xmax
-                    geo['ymax'] = ymax
+                    columns.insert(0, '{0}.st_maxy(SHAPE)'.format(schema, tbl))
+                    columns.insert(0, '{0}.st_maxx(SHAPE)'.format(schema, tbl))
+                    columns.insert(0, '{0}.st_miny(SHAPE)'.format(schema, tbl))
+                    columns.insert(0, '{0}.st_minx(SHAPE)'.format(schema, tbl))
 
-            mapped_cols = job.map_fields(tbl, columns)
-            mapped_cols = dict(zip(mapped_cols, row))
-            entry['id'] = '{0}'.format(mapped_cols['id'])
-            entry['location'] = job.location_id
-            entry['action'] = job.action_type
-            entry['entry'] = {'geo': geo, 'fields': mapped_cols}
-            job.send_entry(entry)
+            rows = job.db_cursor.execute("select {0} from {1}".format(','.join(columns), tbl)).fetchall()
+            for i, row in enumerate(rows):
+                entry = {}
+                if has_shape:
+                    if is_point:
+                        geo['lon'] = row[0]
+                        geo['lat'] = row[1]
+                    else:
+                        geo['xmin'] = row[0]
+                        geo['ymin'] = row[1]
+                        geo['xmax'] = row[2]
+                        geo['ymax'] = row[3]
 
+                mapped_cols = job.map_fields(tbl, columns)
+                mapped_cols = dict(zip(mapped_cols, row))
+
+                if has_shape:
+                    if is_point:
+                        mapped_cols.pop('meta_{0}.st_y(SHAPE)'.format(schema, tbl))
+                        mapped_cols.pop('meta_{0}.st_x(SHAPE)'.format(schema, tbl))
+                    else:
+                        mapped_cols.pop('meta_{0}.st_maxx(SHAPE)'.format(schema, tbl))
+                        mapped_cols.pop('meta_{0}.st_maxy(SHAPE)'.format(schema, tbl))
+                        mapped_cols.pop('meta_{0}.st_minx(SHAPE)'.format(schema, tbl))
+                        mapped_cols.pop('meta_{0}.st_miny(SHAPE)'.format(schema, tbl))
+
+                mapped_cols['_discoveryID'] = job.discovery_id
+                entry['id'] = '{0}_{1}_{2}'.format(job.location_id, tbl, i)
+                entry['location'] = job.location_id
+                entry['action'] = job.action_type
+                entry['entry'] = {'geo': geo, 'fields': mapped_cols}
+                job.send_entry(entry)
+                i += 1
+            print "Total", tbl, i
+        except Exception:
+            pass
 
 def assign_job(job_info):
     """Connects to ZMQ, connects to the database, and assigns the job."""
