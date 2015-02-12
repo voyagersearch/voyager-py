32dbc2eea6c0f0dee11a65bbb83cf8635fe554ca
jason <jason@voyagersearch.com>
2015-02-12 14:26:16 -0800

    VG-1417: Support to index Esri Map/Feature services

--- locations/workers/esri_worker.py
+++ locations/workers/esri_worker.py
@@ -14,11 +14,18 @@
 from __future__ import division
 import os
 import sys
+from collections import OrderedDict
 import logging
 import multiprocessing
 import arcpy
 from utils import status
 
+
+status_writer = status.Writer()
+
+
+class NullGeometry(Exception):
+    pass
 
 def global_job(*args):
     """Create a global job object."""
@@ -37,9 +44,154 @@ def update_row(fields, rows, row):
     return row
 
 
-def worker(data_path):
+def query_layer(layer, spatial_rel='esriSpatialRelIntersects', where='1=1',
+                out_fields='*', out_sr=4326, return_geometry=True):
+    """Returns a GPFeatureRecordSetLayer from a feature service layer or
+    a GPRecordSet form a feature service table.
+    """
+    import _server_admin as arcrest
+    qry_layer = None
+    if layer.type == 'Feature Layer':
+        query = {'spatialRel': spatial_rel, 'where': where,
+                 'outFields': out_fields, 'returnGeometry': return_geometry, 'outSR': out_sr}
+        out = layer._get_subfolder('./query', arcrest.JsonResult, query)
+        qry_layer = arcrest.gptypes.GPFeatureRecordSetLayer.fromJson(out._json_struct)
+    else:
+        query = {'where': where, 'outFields': out_fields, 'returnGeometry': return_geometry}
+        out = layer._get_subfolder('./query', arcrest.JsonResult, query)
+        qry_layer = arcrest.gptypes.GPRecordSet.fromJson(out._json_struct)
+    return qry_layer
+
+
+def make_feature(feature):
+    """Makes a feature from a arcrest.geometry object."""
+    geometry = None
+    if isinstance(feature['geometry'], arcrest.geometry.Polyline):
+        if feature['geometry'].paths:
+            for path in feature['geometry'].paths:
+                point_coords = [(pt.x, pt.y) for pt in path]
+                point_array = arcpy.Array([arcpy.Point(*coords) for coords in point_coords])
+                point_array.add(arcpy.Point())
+            geometry = arcpy.Polyline(point_array)
+    elif isinstance(feature['geometry'], arcrest.geometry.Polygon):
+        if feature['geometry'].rings:
+            for ring in feature['geometry'].rings:
+                point_coords = [(pt.x, pt.y) for pt in ring]
+                point_array = arcpy.Array([arcpy.Point(*coords) for coords in point_coords])
+                point_array.add(arcpy.Point())
+            geometry = arcpy.Polygon(point_array)
+    elif isinstance(feature['geometry'], arcrest.geometry.Multipoint):
+        if feature['geometry'].points:
+            for point in feature['geometry'].points:
+                point_coords = [(pt.x, pt.y) for pt in point]
+                point_array = arcpy.Array([arcpy.Point(*coords) for coords in point_coords])
+                point_array.add(arcpy.Point())
+            geometry = arcpy.Multipoint(point_array)
+
+    if geometry:
+        return geometry
+    else:
+        raise NullGeometry
+
+
+def index_service(url):
+    """Index the records in Map and Feature Services."""
+    try:
+        import _server_admin as arcrest
+    except ImportError as ie:
+        status_writer.send_state(status.STAT_FAILED, ie.message)
+        return
+
+    job.connect_to_zmq()
+    geo = {}
+    entry = {}
+    mapped_attributes = OrderedDict()
+    service = arcrest.FeatureService(url)
+
+    layers = service.layers + service.tables
+
+    for layer in layers:
+        ql = query_layer(layer)
+        if not ql.features:
+            status_writer.send_status("Layer {0} has no features.".format(layer.name))
+            continue
+        if 'attributes' in ql.features[0]:
+            attributes = ql.features[0]['attributes']
+        else:
+            status_writer.send_status("Layer {0} has no attributes.".format(layer.name))
+
+        status_writer.send_status('Indexing {0}...'.format(layer.name))
+        if layer.type == 'Feature Layer':
+            geo['srid'] = ql.spatialReference.wkid
+        if not job.fields_to_keep == ['*']:
+            for fk in job.fields_to_keep:
+                mapped_fields = dict((name, val) for name, val in attributes.items() if fk in name)
+                if job.fields_to_skip:
+                    for fs in job.fields_to_skip:
+                        [mapped_fields.pop(name) for name in attributes if name in fs]
+        else:
+            mapped_fields = attributes
+
+        job.tables_to_keep()  # This call will generate the field mapping dictionary.
+        if 'map' in job.field_mapping[0]:
+            field_map = job.field_mapping[0]['map']
+            for k, v in mapped_fields.items():
+                if k in field_map:
+                    new_field = field_map[k]
+                    mapped_attributes[new_field] = mapped_fields.pop(k)
+                else:
+                    field_type = job.default_mapping(type(v))
+                    mapped_attributes[field_type + k] = mapped_fields.pop(k)
+        else:
+            for k, v in mapped_fields.items():
+                field_type = job.default_mapping(type(v))
+                mapped_attributes[field_type + k] = mapped_fields.pop(k)
+
+        row_count = len(ql.features)
+        if layer.type == 'Table':
+            for i, row in enumerate(ql.features):
+                entry['id'] = '{0}_{1}_{2}'.format(job.location_id, layer.name, i)
+                entry['location'] = job.location_id
+                entry['action'] = job.action_type
+                mapped_fields = dict(zip(mapped_attributes.keys(), row['attributes'].values()))
+                entry['entry'] = {'fields': mapped_fields}
+                job.send_entry(entry)
+                status_writer.send_percent(i / row_count, "{0} {1:%}".format(layer.name, i / row_count), 'esri_worker')
+        else:
+            if isinstance(ql.features[0]['geometry'], arcrest.geometry.Point):
+                for i, feature in enumerate(ql.features):
+                    pt = feature['geometry']
+                    geo['lon'] = pt.x
+                    geo['lat'] = pt.y
+                    entry['id'] = '{0}_{1}_{2}'.format(job.location_id, layer.name, i)
+                    entry['location'] = job.location_id
+                    entry['action'] = job.action_type
+                    mapped_fields = dict(zip(mapped_attributes.keys(), feature['attributes'].values()))
+                    entry['entry'] = {'geo': geo, 'fields': mapped_fields}
+                    job.send_entry(entry)
+                    status_writer.send_percent(i / row_count, "{0} {1:%}".format(layer.name, i / row_count), 'esri_worker')
+            else:
+                for i, feature in enumerate(ql.features):
+                    try:
+                        geometry = make_feature(feature)  # Catch possible null geometries.
+                    except NullGeometry:
+                        continue
+                    geo['xmin'], geo['xmax'] = geometry.extent.XMin, geometry.extent.YMax
+                    geo['ymin'], geo['ymax'] = geometry.extent.YMin, geometry.extent.YMax
+                    entry['id'] = '{0}_{1}_{2}'.format(job.location_id, layer.name, i)
+                    entry['location'] = job.location_id
+                    entry['action'] = job.action_type
+                    mapped_fields = dict(zip(mapped_attributes.keys(), feature['attributes'].values()))
+                    entry['entry'] = {'geo': geo, 'fields': mapped_fields}
+                    job.send_entry(entry)
+                    status_writer.send_percent(i / row_count, "{0} {1:%}".format(layer.name, i / row_count), 'esri_worker')
+
+
+def worker(data_path, esri_service=False):
     """The worker function to index feature data and tabular data."""
-    if data_path:
+    if esri_service:
+        index_service(data_path)
+    else:
         job.connect_to_zmq()
         geo = {}
         entry = {}
@@ -62,7 +214,6 @@ def worker(data_path):
                 for i, row in enumerate(rows, 1):
                     if job.domains:
                         row = update_row(dsc.fields, rows, list(row))
-                    #mapped_fields = job.map_fields(dsc.name, fields, field_types)
                     mapped_fields = dict(zip(mapped_fields, row))
                     mapped_fields['_discoveryID'] = job.discovery_id
                     mapped_fields['title'] = dsc.name
@@ -104,7 +255,6 @@ def worker(data_path):
                         geo['lat'] = row[0].firstPoint.Y #row[0][1]
                         if job.include_wkt:
                             geo['wkt'] = row[0].WKT
-                        #mapped_fields = job.map_fields(dsc.name, list(rows.fields[1:]), field_types)
                         mapped_fields = dict(zip(mapped_fields, row[1:]))
                         mapped_fields['_discoveryID'] = job.discovery_id
                         mapped_fields['title'] = dsc.name
@@ -126,7 +276,6 @@ def worker(data_path):
                         geo['ymax'] = row[0].extent.YMax
                         if job.include_wkt:
                             geo['wkt'] = row[0].WKT
-                        #mapped_fields = job.map_fields(dsc.name, list(rows.fields[1:]), field_types)
                         mapped_fields = dict(zip(mapped_fields, row[1:]))
                         mapped_fields['_discoveryID'] = job.discovery_id
                         mapped_fields['title'] = dsc.name
@@ -140,9 +289,14 @@ def worker(data_path):
 
 def run_job(esri_job):
     """Determines the data type and each dataset is sent to the worker to be processed."""
-    status_writer = status.Writer()
     status_writer.send_percent(0.0, "Initializing... 0.0%", 'esri_worker')
     job = esri_job
+
+    if job.path.startswith('http'):
+        global_job(job)
+        worker(job.path, esri_service=True)
+        return
+
     dsc = arcpy.Describe(job.path)
 
     # A single feature class or table.
