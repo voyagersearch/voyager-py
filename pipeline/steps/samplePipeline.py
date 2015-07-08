4e841fa41943f97e3499c1999cec120a9df1a78d
jason <jason@voyagersearch.com>
2015-07-08 08:49:49 -0700

    code improvements

--- pipeline/steps/samplePipeline.py
+++ pipeline/steps/samplePipeline.py
@@ -1,19 +1,16 @@
 import sys
 import json
-from copy import deepcopy
 
 
 def run(entry):
-
     """
-    Sample Python pipeline step. Searches for "Voyager" or "voyager" and returns the word count.
+    Sample Python pipeline step. Searches the text field for "Voyager" or "voyager" and returns the word count.
     :param entry: a JSON file containing a voyager entry.
     """
-    orig_entry = json.load(open(entry, "rb"))
-    new_entry = deepcopy(orig_entry)
+    new_entry = json.load(open(entry, "rb"))
     voyager_word_count = 0
-    if 'fields' in orig_entry and 'text' in orig_entry['fields']:
-        text_field = orig_entry['fields']['text']
+    if 'fields' in new_entry and 'text' in new_entry['fields']:
+        text_field = new_entry['fields']['text']
         voyager_word_count += text_field.Count('Voyager')
         voyager_word_count += text_field.Count('voyager')
         new_entry['fields']['fi_voyager_word_count'] = voyager_word_count
