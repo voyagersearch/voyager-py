b241178180ca339f0e1f3ba0dedb0a60033094e1
jason <jason@voyagersearch.com>
2015-11-18 13:02:48 -0800

    VG-3450: map fields to correct data type

--- extractors/vgextractors/AvroExtractor.py
+++ extractors/vgextractors/AvroExtractor.py
@@ -1,9 +1,29 @@
 import os
+import datetime
 import fastavro
 from _vgdexfield import VgDexField
 
 
 class AvroExtractor(object):
+
+    @staticmethod
+    def format_date(date_object):
+        # Format date to iso 8601
+        if isinstance(date_object, str):
+            dt = datetime.datetime.strptime(date_object, "%Y%m%d%H%M%S")
+            return dt.strftime('%Y-%m-%dT%H:%M:%S.%f%Z')
+        else:
+            try:
+                if date_object.tzinfo:
+                    return date_object.strftime('%Y-%m-%dT%H:%M:%S.%f%Z')
+                else:
+                    return date_object.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            except ValueError:
+                # For dates before 1900.
+                date_str = datetime.datetime.isoformat(date_object) + 'Z'
+                return date_str
+            except Exception:
+                return list(date_object.timetuple())[0:6]
 
     @staticmethod
     def extractor():
@@ -36,7 +56,26 @@ class AvroExtractor(object):
                             maxy = float(v)
                         elif k == 'MBR_SOUTH' and v:
                             miny = float(v)
-                    job.set_field("meta_{0}".format(k), v)
+
+                    # Map values to correct data type.
+                    if isinstance(v, str):
+                        job.set_field("fs_{0}".format(k), v)
+                    elif isinstance(v, unicode):
+                        job.set_field("fs_{0}".format(k), v)
+                    elif isinstance(v, bool):
+                        job.set_field("fs_{0}".format(k), v)
+                    elif isinstance(v, int):
+                        job.set_field("fl_{0}".format(k), v)
+                    elif isinstance(v, float):
+                        job.set_field("fu_{0}".format(k), v)
+                    elif isinstance(v, datetime.datetime):
+                        job.set_field("fd_{0}".format(k), self.format_date(v))
+                    elif (v and "Date" in k) or (isinstance(v, unicode) and len(v.strip()) == 14):
+                        job.set_field("fd_{0}".format(k), self.format_date(v))
+                    elif isinstance(v, list):
+                        job.set_field("fs_{0}".format(k), v)
+                    else:
+                        job.set_field("meta_{0}".format(k), v)
 
             if minx and not poly_wkt:
                 poly_wkt = "POLYGON (({0} {1}, {0} {3}, {2} {3}, {2} {1}, {0} {1}))".format(minx, miny, maxx, maxy)
