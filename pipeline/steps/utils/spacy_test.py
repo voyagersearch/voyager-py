1c0151ee0aa64a299daf0573708b4c7dcf59fae0
nebulae <nebulae@gmail.com>
2017-03-15 13:33:51 -0700

    added script to test that the space module is installed, accessed from the java plugin

---
+++ pipeline/steps/utils/spacy_test.py
@@ -0,0 +1,13 @@
+"""
+Simple script to test the existance of the spacy module
+Called from the java plugin.
+"""
+import sys
+
+try:
+    import spacy
+    sys.stdout.write("%s" % True)
+except Exception as e:
+    sys.stdout.write("%s" % False)
+    sys.stdout.write(e)
+
