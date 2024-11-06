02e12c68ad4bf69eb753b6eb66252719e87ac0d1
jason <jason@voyagersearch.com>
2024-11-06 10:31:17 -0800

    VG-9445: Fix for delete items task. Ensures query is formatted & correctly returning expected results before delete.

--- processing/tasks/delete_items.py
+++ processing/tasks/delete_items.py
@@ -14,6 +14,10 @@
 # limitations under the License.
 import os
 import sys
+try:
+    import urllib.parse
+except ImportError:
+    from urlparse import urlparse, parse_qs
 import requests
 from utils import status
 from utils import task_utils
@@ -29,18 +33,39 @@ status_writer = status.Writer()
 errors_reasons = {}
 
 
-def delete_items(fq_query, q_query, thumbs, metadata, layers, owner):
+def delete_items(fq_query, q_query, thumbs, metadata, layers, owner, result_count):
     """Delete items from the index using the Voyager API."""
     try:
         voyager_server = sys.argv[2].split('=')[1].split('solr')[0][:-1]
-        # voyager_server = "http://localhost:8888"
+        # voyager_server = "https://windows10/voyager/"
         if not q_query and fq_query:
             query = fq_query
             fq = ""
         else:
             query = q_query
             fq = "&fq={0}".format(fq_query)
+        try:
+            query = urllib.parse.quote(query)
+        except Exception:
+            parsed_url = urlparse(query)
+            query = parse_qs(parsed_url.query)
 
+        # Perform the query and ensure the result count is as expected before deleting items from the index.
+        search_query = '{0}{1}{2}'.format(sys.argv[2].split('=')[1], '/select?&rows=1&wt=json&q=', query)
+        search_res = requests.get(search_query, verify=verify_ssl, headers={'Content-type': 'application/json', 'x-access-token': task_utils.get_security_token(owner)})
+        num_found = search_res.json()["response"]["numFound"]
+        # More encoding may be necessary for escape characters
+        if not num_found == result_count:
+            query = query.replace("%20", "%5C%20")
+            search_query = '{0}{1}&q={2}'.format(sys.argv[2].split('=')[1], '/select?&rows=1&wt=json', query)
+            search_res = requests.get(search_query, verify=verify_ssl, headers={'Content-type': 'application/json', 'x-access-token': task_utils.get_security_token(owner)})
+            num_found = search_res.json()["response"]["numFound"]
+            # Query is not returning expected results. Will not perform deletion of items.
+            if not num_found == result_count:
+                status_writer.send_state(status.STAT_FAILED, "Invalid query. Unable to perform delete.")
+                return False, 'Error deleting items: {0}: {1}'.format('delete_items', "Invalid query. Unable to perform delete.")
+
+        # Query is valid and perform delete.
         url = "{0}/api/rest/index/records?query={1}{2}&items=true&thumbnails={3}&metadata={4}&layers={5}".format(voyager_server, query, fq, thumbs, metadata, layers)
         response = requests.delete(url, verify=verify_ssl, headers={'Content-type': 'application/json', 'x-access-token': task_utils.get_security_token(owner)})
         if response.status_code == 200:
@@ -88,7 +113,7 @@ def execute(request):
     if 'q' in parameters[response_index]['query']:
         query = parameters[response_index]['query']['q']
 
-    result = delete_items(fq, query, delete_thumbs, delete_metadata, delete_layers, request_owner)
+    result = delete_items(fq, query, delete_thumbs, delete_metadata, delete_layers, request_owner, result_count)
 
     if not result[0]:
         errors += 1
